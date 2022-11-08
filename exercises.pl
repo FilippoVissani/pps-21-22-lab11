44c0a6aac66be54fdf29f7f7f72918ebf7fb648b
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-11-08 11:59:01 +0100

    Added exercise 2.5

--- exercises.pl
+++ exercises.pl
@@ -50,3 +50,18 @@ fromCircList([E1, E2|T], H, [e(E1, E2)|L]) :- fromCircList([E2|T], H, L).
 in_degree([], _, 0).
 in_degree([e(_,X)|T], N, C) :- X \= N, in_degree(T, N, C).
 in_degree([e(_,N)|T], N, C) :- in_degree(T, N, C2), C is C2 + 1.
+
+% exercise 2.4
+% dropNode(+Graph, +Node, -OutGraph)
+% drop all edges starting and leaving from a Node
+% use dropAll defined in 1.1??
+dropNode(G, N, OG) :- dropAll(G, e(N, _), G2), dropAll(G2, e(_, N), GO).
+
+% exercise 2.5
+% reaching (+ Graph , + Node , - List )
+% all the nodes that can be reached in 1 step from Node
+% possibly use findall , looking for e ( Node , _ ) combined
+% with member (? Elem ,? List )
+% reaching([e(1,2),e(1,3),e(2,3)],1,L). -> L/[2,3]
+% reaching([e(1,2),e(1,2),e(2,3)],1,L). -> L/[2,2])
+reaching(G, N, L) :- findall(N2, member(e(N, N2), G), L).
